7eab5d71f5ddc0fd1df84da8785f0888bdb26ae6
Huw Bellringer <dhbellringer@msn.com>
2022-08-30 01:18:47 +0100

    Simple setup and initial frame rendering

---
+++ project_1/script.js
@@ -0,0 +1,29 @@
+const canvas = document.getElementById("canvas1");
+const ctx = canvas.getContext("2d");
+const CANVAS_WIDTH = (canvas.width = 600);
+const CANVAS_HEIGHT = (canvas.height = 600);
+
+const playerImage = new Image();
+playerImage.src = "assets/shadow_dog.png";
+const spriteWidth = 575;
+const spriteHeight = 523;
+
+function animate() {
+  ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  // ctx.fillRect(50, 50, 100, 100);
+  ctx.drawImage(
+    playerImage,
+    0,
+    0,
+    spriteWidth,
+    spriteHeight,
+    0,
+    0,
+    spriteWidth,
+    spriteHeight
+  );
+
+  requestAnimationFrame(animate);
+}
+
+animate();

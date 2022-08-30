6dd9506c7a0cd031c90568c712eb38937c1a4269
Huw Bellringer <dhbellringer@msn.com>
2022-08-30 01:32:42 +0100

    Added frame rate reduction, and hardcoded method to use correct frame count when animating

--- project_1/script.js
+++ project_1/script.js
@@ -7,14 +7,18 @@ const playerImage = new Image();
 playerImage.src = "assets/shadow_dog.png";
 const spriteWidth = 575;
 const spriteHeight = 523;
+let frameX = 0;
+let frameY = 0;
+let gameFrame = 0;
+const staggerFrames = 3;
+let frameCount = [7, 7, 7, 9, 11, 5, 7, 7, 12, 4];
 
 function animate() {
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-  // ctx.fillRect(50, 50, 100, 100);
   ctx.drawImage(
     playerImage,
-    0,
-    0,
+    spriteWidth * frameX,
+    spriteHeight * frameY,
     spriteWidth,
     spriteHeight,
     0,
@@ -22,7 +26,10 @@ function animate() {
     spriteWidth,
     spriteHeight
   );
-
+  if (gameFrame % staggerFrames == 0) {
+    frameX = (frameX + 1) % frameCount[frameY];
+  }
+  gameFrame++;
   requestAnimationFrame(animate);
 }
 
